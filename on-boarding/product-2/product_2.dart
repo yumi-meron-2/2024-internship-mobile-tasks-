efcb2cca6dfe45ca9a0f2edab8c3921abe74c803
yumi-meron-2 <meron.weldu.t@gmail.com>
2024-07-29 22:37:33 +0200

    Add product_2.dart to on-boarding/product-2

--- on-boarding/product-2/product_2.dart
+++ on-boarding/product-2/product_2.dart
@@ -2,10 +2,14 @@ import 'dart:ffi';
 import 'dart:io';
 
 void main() {
+  // initializing the eCommerce object
   Product_Manager product_manager = Product_Manager();
   
   String? input;
+
+  // while loop for letting user to use the service as long as they like too
   do{
+    // prompt questions 
     print('''
           What Do You Want To Do?\n
           A) Add Product.\n
@@ -18,38 +22,53 @@ void main() {
 
     String? input = stdin.readLineSync();
     
+    // if they want to exit
     if (input == null || input == 'F'){
       return ;
     }
     
+    // if they want to add a product
     else if (input.toUpperCase() == 'A'){
+
       print("Enter Product Name: ");
       String? name = stdin.readLineSync();
+
       if (name == null){
         print("Invalid Input");
         continue;
       }
+
       print('Enter product description');
       String? description = stdin.readLineSync();
+
       print('Enter Product Price: ');
       String? price_str = stdin.readLineSync();
+
       double price = 0.00;
       if (price_str != null){
         price = double.parse(price_str);
       }
+
+      // initializing our product object
       Product product = Product(name, description, price);
+      // calling the function to add product
       product_manager.add_product(product);
       
       }
+
+    //if they want to display all products
     else if (input.toUpperCase() == 'B'){
       product_manager.view_all_products();
       }
+
+    // if they want to display specific product
     else if ( input.toUpperCase() == 'C'){
       print("Enter Product Name To Be Displayed: ");
       String? name = stdin.readLineSync();
       product_manager.view_single_product(name);
     }
 
+    //if they want to update specific product
     else if (input.toUpperCase() == 'D'){
       print("Enter Product Name To Be Updated: ");
       String? name = stdin.readLineSync();
@@ -67,19 +86,24 @@ void main() {
       if (price_str != null){
         price = double.parse(price_str);
       }
+
       product_manager.update_product(name, new_name, description, price);
 
     }
 
+    // if they want to delete specific product
     else if (input.toUpperCase() == 'E'){
       print("Enter Product Name To Be Deleted: ");
       String? name = stdin.readLineSync();
       product_manager.delete_product(name);
       
     }
+
     else{
       print('Invalid Input.');
     }
+
+    // prompting user if they want to continue using the servies
     print('Do you want to continue:\n say \'yes\' or \'no\' ');
     String? continueInput = stdin.readLineSync();
     if (continueInput == null || continueInput.toLowerCase() != 'yes') {
@@ -91,56 +115,71 @@ void main() {
 
     }
 
-    class Product{
-        
-        String? name, description;
-        double? price;
 
-        Product(String? name, String? description, double price){
-            this.name = name;
-            this.description = description;
-            this.price = price;
-        }
-
+// product class
+class Product{
     
+    String? name, description;
+    double? price;
+
+    Product(String? name, String? description, double price){
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+
 }
 
+
+// eCommerce class
 class Product_Manager{
+    //list to hold all products
     List<Product> products = [];
 
+    // function to display product
     void display(product){
       String? name = product.name;
         String? description = product.description;
         double? price = product.price;
         print("Product Name: $name \t Description: $description \t Price: $price");
     }
+
+    // function to add product to list
     void add_product(Product product){
         products.add(product);
         print('Product Added Successfully');
     }
+
+    // function to display all products
     void view_all_products(){
       for (var item in products){
         display(item);
       }
     }
+
+    // function to display single element
     void view_single_product(String? name){
       bool flag = false;
       for (var item in products){
         if (item.name == name){
-          flag = true;
+          flag = true; //setting it to true to imply product is found
           display(item);
           break;
         }
       }
+      //incase product is not found
       if (flag == false){
         print('Product Not Found');
       }
     }
 
+    // function to update specific product
     void update_product(String? name, String? new_name, String? description, double price){
       bool flag = false;
       for (var item in products){
         if (item.name == name){
+          flag = true; //setting it to true to imply product is found
           item.name = new_name;
           item.description = description;
           item.price = price;
@@ -148,16 +187,16 @@ class Product_Manager{
           break;
         }
       }
+      // incase product is not found
       if (flag == false){
         print('Product Not Found');
       }
     }
 
+    // dunction to delete specific product
     void delete_product(String? name){
-      
       products.removeWhere((item) => item.name == name);
       print('Product Deleted Successfully.');
-      
       }
     
 }
